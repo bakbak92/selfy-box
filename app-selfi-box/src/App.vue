eb3364893f344e8c34fa657eab802cbd36f5f4ce
Bakary COLY <bakarycoly92@gmail.com>
2018-07-13 22:08:31 +0200

    build

--- app-selfi-box/src/App.vue
+++ app-selfi-box/src/App.vue
@@ -69,7 +69,7 @@ export default {
       ],
       icons: [
         'fab fa-facebook',
-        'fab fa-twitter',
+        'fab fa-twitter ',
         'fab fa-google-plus',
         'fab fa-linkedin',
         'fab fa-instagram'
